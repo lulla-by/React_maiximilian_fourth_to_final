d5007fd23f18750efa660d83e408f374eddaa1bb
lulla-by <thfl3363@hanmail.net>
2023-05-04 23:22:47 +0900

    사용자 입력 state관리

--- 04.React_validation-Modal/src/Components/Users/AddUser.jsx
+++ 04.React_validation-Modal/src/Components/Users/AddUser.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
-import Card from '../UI/Card'
-import Button from '../UI/Button'
-import classes from "./AddUser.module.css"
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-console.log(classes.input)
-  const addUserHandler = e =>{
-    e.preventDefault()
-  }
+  //구조분해할당
+  let [enteredUserName, setEnteredUserName] = useState("");
+  let [enteredUserAge, setEnteredUserAge] = useState("");
+  const addUserHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredUserName, enteredUserAge);
+    setEnteredUserAge("");
+    setEnteredUserName("");
+  };
+  const userNameChangeHandeler = (e) => {
+    setEnteredUserName(e.target.value);
+  };
+  const userAgeChangeHandeler = (e) => {
+    setEnteredUserAge(e.target.value);
+  };
 
   return (
     <Card className={classes.input}>
-    <form onSubmit={addUserHandler }>
-      <label htmlFor="username">User Name</label>
-      <input id='username' type="text" />
-      <label htmlFor="age">Age(Years)</label>
-      <input id='age' type="number" />
-      <Button type='submit'>Add User</Button>
-    </form>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">User Name</label>
+        <input
+          value={enteredUserName}
+          id="username"
+          type="text"
+          onChange={userNameChangeHandeler}
+        />
+        <label htmlFor="age">Age(Years)</label>
+        <input
+          value={enteredUserAge}
+          id="age"
+          type="number"
+          onChange={userAgeChangeHandeler}
+        />
+        <Button onClick={addUserHandler} type="submit">
+          Add User
+        </Button>
+      </form>
     </Card>
-  )
-}
+  );
+};
 
-export default AddUser
+export default AddUser;
