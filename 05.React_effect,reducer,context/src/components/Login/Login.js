874e55819df0392834cc84164672c479107b8074
lulla-by <thfl3363@hanmail.net>
2023-05-21 18:40:30 +0900

    입력 컴포넌트 리팩토링(컴포넌트 재사용) - 컴포넌트를 재사용하는겅우에는 context가 아닌 props를 사용하는것이 좋음

--- 05.React_effect,reducer,context/src/components/Login/Login.js
+++ 05.React_effect,reducer,context/src/components/Login/Login.js
@@ -1,66 +1,65 @@
-import React, { useState ,useEffect,useReducer,useContext} from 'react';
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
-import AuthContext from '../../store/auth-context';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
+import Input from "../UI/Input/Input";
 
-const emailReducer = (state,action) =>{
+const emailReducer = (state, action) => {
   // console.log(`state`,state)
   // console.log("action",action)
-  if(action.type === "USER_INPUT"){
+  if (action.type === "USER_INPUT") {
     //이메일의 state snaptshot반환
     // console.log({value:action.val, isValid:action.val.includes("@")})
-    return {value:action.val, isValid:action.val.includes("@")}
+    return { value: action.val, isValid: action.val.includes("@") };
     //  USER_INPUT 액션을 받을때마다 value와 isvalid를 모두 업데이트
-    
   }
-  if(action.type === "INPUT_BLUR"){
+  if (action.type === "INPUT_BLUR") {
     // 이전에 받았던 값을 state로 반환
-    return {value:state.value,isValid:state.value.includes("@")}
+    return { value: state.value, isValid: state.value.includes("@") };
   }
   // 빈 snapshot
-  return {value:"",isValid:false}
+  return { value: "", isValid: false };
   // 이 리듀서로 오는 다른 모든 액션에 대해 이 기본 state를 반환
-}
+};
 
-
-const passWordReducer =(state,action) => {
+const passWordReducer = (state, action) => {
   //return값이 해당 리듀서의 state로 반환됨, 여기서는 54번재 줄의 emailState값
-  
-  if(action.type === "USER_INPUT"){
-    return {value:action.val,isValid:action.val.trim().length > 6}
-    
-  }
-  if(action.type === "INPUT_BLUR"){
 
-    return {value:state.value,isValid:state.value.trim().length > 6}
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
   }
-  return {value:"",isValid:false}
-}
-
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: "", isValid: false };
+};
 
 const Login = (props) => {
-
-
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  
+
   // const [enteredPassword, setEnteredPassword] = useState('');
   // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   //                                  useReducer(함수, 리듀서의 기본 state 객체)
-  const [emailState, dispatchEmail] = useReducer(emailReducer,{value:"",isValid:undefined})
-  const [passwordState, dispatchPassword] = useReducer(passWordReducer,{value:"",isValid:undefined })
-
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: undefined,
+  });
+  const [passwordState, dispatchPassword] = useReducer(passWordReducer, {
+    value: "",
+    isValid: undefined,
+  });
 
   // 구조분해할당 + alias assignment
-  const {isValid:emailIsValid} = emailState
-  const {isValid:passwordIsValid} = passwordState
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
 
-  const ctx = useContext(AuthContext)
-  useEffect(()=>{
+  const ctx = useContext(AuthContext);
+  useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("checking for validity");
       // console.log("이메일 유효성",emailIsValid)
@@ -69,20 +68,19 @@ const Login = (props) => {
         emailState.isValid && passwordState.isValid
       );
     }, 500);
-    return()=>{
-      console.log("CLEANUP")
-      clearTimeout(identifier)
-    }
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    };
 
     // 각 객체의 특정한 값들(isValid)를 구조분해할당으로 추출해서 해당 부분을 종속성으로 추가
     // 이메일과 패스워드의 true false값이 바뀔때만 해당 사이드이펙트가 실행됨
-  },[emailIsValid,passwordIsValid])
-
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
-    // emailReducer에 해당 객체를 action으로 보내줌. 
+    // emailReducer에 해당 객체를 action으로 보내줌.
     // 필드는 대부분 명명된 type, val은 payload
-    dispatchEmail({type:"USER_INPUT",val:event.target.value});
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
     // setFormIsValid(
     //   event.target.value.includes('@') && passwordState.value.trim().length > 6
@@ -92,32 +90,22 @@ const Login = (props) => {
 
   const passwordChangeHandler = (event) => {
     // setEnteredPassword(event.target.value);
-    dispatchPassword({type:"USER_INPUT",val:event.target.value})
-    
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+
     // setFormIsValid(
     //  emailState.value.includes('@')  && event.target.value.trim().length > 6
     // );
     // 여전히 폼 유효성을 다른 state에서 도출함 => 여기서는 emailState => 코드가 최적의 상태는 아님
   };
 
-
-
-
-
   const validateEmailHandler = () => {
-    dispatchEmail({type:"INPUT_BLUR"})
+    dispatchEmail({ type: "INPUT_BLUR" });
   };
 
   const validatePasswordHandler = () => {
     // setPasswordIsValid(enteredPassword.trim().length > 6);
-    dispatchPassword({type:"INPUT_BLUR"})
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
-
-
-
-
-
-
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -127,34 +115,25 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ''
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+          id="e-mail"
+          label="E-Mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <Input
+          id="password"
+          label="password"
+          type="password"
+          isValid={passwordIsValid}
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
+
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
