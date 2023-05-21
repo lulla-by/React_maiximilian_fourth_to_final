3bc45d43b0a89cacd98d8d67154512d2f2c2c08d
lulla-by <thfl3363@hanmail.net>
2023-05-21 13:36:20 +0900

    오타 수정

--- 05.React_effect,reducer,context/src/components/Login/Login.js
+++ 05.React_effect,reducer,context/src/components/Login/Login.js
@@ -4,42 +4,44 @@ import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state,action) =>{
+  // console.log(`state`,state)
+  // console.log("action",action)
+  if(action.type === "USER_INPUT"){
+    //이메일의 state snaptshot반환
+    // console.log({value:action.val, isValid:action.val.includes("@")})
+    return {value:action.val, isValid:action.val.includes("@")}
+    //  USER_INPUT 액션을 받을때마다 value와 isvalid를 모두 업데이트
+    
+  }
+  if(action.type === "INPUT_BLUR"){
+    // 이전에 받았던 값을 state로 반환
+    return {value:state.value,isValid:state.value.includes("@")}
+  }
+  // 빈 snapshot
+  return {value:"",isValid:false}
+  // 이 리듀서로 오는 다른 모든 액션에 대해 이 기본 state를 반환
+}
+
+
+const passWordReducer =(state,action) => {
+  console.log("state",state)
+  console.log("action",action)
+  if(action.type === "USER_INPUT"){
+    return {value:action.val,isValid:action.val.trim().length > 6}
+    
+  }
+  if(action.type === "INPUT_BLUR"){
+
+    return {value:state.value,isValid:state.value.trim().length > 6}
+  }
+  return {value:"",isValid:false}
+}
+
+
 const Login = (props) => {
   console.log(props)
 
-  const emailReducer = (state,action) =>{
-    // console.log(`state`,state)
-    // console.log("action",action)
-    if(action.type === "USER_INPUT"){
-      //이메일의 state snaptshot반환
-      // console.log({value:action.val, isValid:action.val.includes("@")})
-      return {value:action.val, isValid:action.val.includes("@")}
-      //  USER_INPUT 액션을 받을때마다 value와 isvalid를 모두 업데이트
-      
-    }
-    if(action.type === "INPUT_BLUR"){
-      // 이전에 받았던 값을 state로 반환
-      return {value:state.value,isValid:state.value.includes("@")}
-    }
-    // 빈 snapshot
-    return {value:"",isValid:false}
-    // 이 리듀서로 오는 다른 모든 액션에 대해 이 기본 state를 반환
-  }
-
-
-  const passWordReducer =(state,action) => {
-    console.log("state",state)
-    console.log("action",action)
-    if(action.type === "USER_INPUT"){
-      return {value:action.val,isValid:action.val.trim().length > 6}
-      
-    }
-    if(action.type === "INPUT_BLUR"){
-
-      return {value:state.value,isValid:state.value.trim().length > 6}
-    }
-    return {value:"",isValid:false}
-  }
 
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
@@ -129,7 +131,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="password">Password</label>
