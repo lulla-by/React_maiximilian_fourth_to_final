02dd73843d0a0f6630739d67655bb22cf906476a
lulla-by <thfl3363@hanmail.net>
2023-05-22 20:37:38 +0900

    추가시 기존 장바구니에 수량만 추가 되도록 구현

--- 06.React(BuildingFoodOrderApp)/src/store/CartProvider.js
+++ 06.React(BuildingFoodOrderApp)/src/store/CartProvider.js
@@ -14,8 +14,23 @@ const defaultCartState = {
 // 2. 컴포넌트 외부에서 CartReducer함수 추가 => 이 리듀서 함수는 그 컴포넌트에서 아무것도 필요로 하지 않음
 const cartReduce = (state, action) => {
   if(action.type === "ADD"){
-    const updatedItems = state.items.concat(action.item);
     const newTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const existingCartIndex = state.items.findIndex(item => item.id === action.item.id )
+    const existingCartItem = state.items[existingCartIndex]
+
+    let updatedItems;
+    
+    if(existingCartItem){
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      }
+      updatedItems = [...state.items]
+      updatedItems[existingCartIndex] = updatedItem;
+    }else{
+      updatedItems=state.items.concat(action.item);
+    }
+
     return {items:updatedItems, totalAmount:newTotalAmount }
   }
   return defaultCartState;
