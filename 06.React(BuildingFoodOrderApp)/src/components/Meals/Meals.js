8b437de6fc01fa2b0c4d2e764d1a6c3e41d60fc1
lulla-by <thfl3363@hanmail.net>
2023-05-22 10:50:48 +0900

    식사 컴포넌트 추가

---
+++ 06.React(BuildingFoodOrderApp)/src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MealsSummary from './MealsSummary';
+import AvaliableMeals from './AvaliableMeals';
+
+const Meals = () => {
+  return (
+<React.Fragment>
+ < MealsSummary/>
+ <AvaliableMeals/>
+</React.Fragment>
+  )
+}
+
+export default Meals
