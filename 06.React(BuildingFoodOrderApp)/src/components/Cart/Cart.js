46b37032aefc86eed594b2c802be40aa02d2c57f
lulla-by <thfl3363@hanmail.net>
2023-05-22 20:21:26 +0900

    장바구니 항목 추가하기

--- 06.React(BuildingFoodOrderApp)/src/components/Cart/Cart.js
+++ 06.React(BuildingFoodOrderApp)/src/components/Cart/Cart.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  console.log(cartCtx);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItem = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
-    <ul className={classes['cart-items']}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((itme) => (
-        <li>{itme.name}</li>
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          // 이렇게 하면 추가되거나 삭제된 항목의 id가 remove 핸들러로 전달
+          onRemove={cartItemRemoveHandler.bind(null,item.id)}
+          onAdd = {cartItemAddHandler.bind(null,item.id)}
+        />
       ))}
     </ul>
   );
@@ -14,12 +32,14 @@ const Cart = (props) => {
     <Modal onClick={props.onHideCart}>
       {cartItems}
       <div className={classes.total}>
-      <span>Total Amount</span>
-      <span>35.62</span>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>
+          Close
+        </button>
+        {hasItem && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- 06.React(BuildingFoodOrderApp)/src/store/CartProvider.js
+++ 06.React(BuildingFoodOrderApp)/src/store/CartProvider.js
@@ -13,7 +13,6 @@ const defaultCartState = {
 
 // 2. 컴포넌트 외부에서 CartReducer함수 추가 => 이 리듀서 함수는 그 컴포넌트에서 아무것도 필요로 하지 않음
 const cartReduce = (state, action) => {
-  console.log(action)
   if(action.type === "ADD"){
     const updatedItems = state.items.concat(action.item);
     const newTotalAmount = state.totalAmount + action.item.price * action.item.amount;
