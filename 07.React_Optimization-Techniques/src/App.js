4f5bf51079d577c65dee9b5e9ffe27784f8df668
lulla-by <thfl3363@hanmail.net>
2023-05-24 16:28:48 +0900

    useCallback으로 함수 재생성 방지

--- 07.React_Optimization-Techniques/src/App.js
+++ 07.React_Optimization-Techniques/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useCallback} from 'react';
 
 import './App.css';
 import Button from './components/UI/Button/Button';
@@ -9,9 +9,14 @@ function App() {
   const [showParagraph,setShowParagraph] = useState()
  
   console.log("1. App Running")
-  const toggleParagraphHandler = () =>{
+
+  // useCallback
+  // 컴포넌트 실행 전반에 걸쳐 함수를 저장할 수 있게하는 훅
+  // 리액트에 원하는 함수를 저장하고 매번 실행때마다 이 함수를 재생성할 필요가 없다는 것을 알림
+  // => 동일한 함수객체가 메모리의 동일한 위치에 저장, 이를 통한 비교작업이 가능
+  const toggleParagraphHandler = useCallback(() =>{
     setShowParagraph(prevShowParagraph => !prevShowParagraph)
-  }
+  },[])
   return (
     <div className="app">
       <h1>Hi there!</h1>
--- 07.React_Optimization-Techniques/src/components/UI/Button/Button.js
+++ 07.React_Optimization-Techniques/src/components/UI/Button/Button.js
@@ -3,21 +3,6 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = (props) => {
-  // React.memo로 감싸고 프롭스로 받는 함수(toggleParagraph)와 children모두 같지만 재평가가 이루어짐 왜일까
-  // 매우 흔한 오류 중 하나
-  // 자바스크립트에서는 app함수가 실행될때마다 만들어지는 함수는 모두 새로운 함수!
-  // 그렇다면 왜 DemoOutput에서는 작동하고 여기스ㅓ는 왜 안작동하는가...
-
-  // DemoOutput에서 props로 받는 false는 boolean타입으로 원시값임
-  // React.memo는 사실상 이전의 값과 현재의 값을 비교하는 것으로 
-  // 원시값인 false === false  => true가 나옴
-  
-  // 하지만 배열이나 객체, 함수를 비교하면 말이 달라짐
-  // [1,2,3] === [1,2,3]  => false
-
-  // 여기에서는 React.memo를 통해
-  // props.onclick === props.previous.onClick  => false
-
   console.log("4. Button Running")
   return (
     <button
