a07793407fc11b9799fd6c54d70a1b70f198c1ba
lulla-by <thfl3363@hanmail.net>
2023-05-24 16:40:32 +0900

    useCallback 및 해당 종속성

--- 07.React_Optimization-Techniques/src/App.js
+++ 07.React_Optimization-Techniques/src/App.js
@@ -7,6 +7,7 @@ import DemoOutput from './components/Demo/DemoOutput';
 function App() {
  
   const [showParagraph,setShowParagraph] = useState()
+  const [allowToggle,setAllowToggle] = useState()
  
   console.log("1. App Running")
 
@@ -15,12 +16,20 @@ function App() {
   // 리액트에 원하는 함수를 저장하고 매번 실행때마다 이 함수를 재생성할 필요가 없다는 것을 알림
   // => 동일한 함수객체가 메모리의 동일한 위치에 저장, 이를 통한 비교작업이 가능
   const toggleParagraphHandler = useCallback(() =>{
-    setShowParagraph(prevShowParagraph => !prevShowParagraph)
-  },[])
+    if(allowToggle){
+      setShowParagraph(prevShowParagraph => !prevShowParagraph)
+    }
+  },[allowToggle])
+
+
+  const allowToggleHandler = () =>{
+    setAllowToggle(true)
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
-     <DemoOutput show={false}/>
+     <DemoOutput show={showParagraph}/>
+      <Button onClick={allowToggleHandler} >Allow Toggling!</Button>
       <Button onClick={toggleParagraphHandler} >Show Paragraph!</Button>
     </div>
   );
