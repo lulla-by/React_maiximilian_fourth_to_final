add3b1cf6a2e31f189f2f6f2e5176a1e4bba8f45
lulla-by <thfl3363@hanmail.net>
2023-05-25 12:30:52 +0900

    componentWillUnmount()

--- 08.React_class_components/src/components/User.js
+++ 08.React_class_components/src/components/User.js
@@ -2,7 +2,11 @@ import classes from './User.module.css';
 import {Component} from "react"
 
 class User extends Component{
-
+componentWillUnmount(){
+  console.log("user will unmount!")       
+  //  hide users클릭시 user will unmount가 세번 호출 
+  // 3명을 렌더링하고 각각의 인스턴스들이 모두 삭제되기때문
+}
 
 render(){
   // console.log(this)
