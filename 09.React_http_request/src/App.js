7e59e678109430eccedf38b5978a0fde328d8ae1
lulla-by <thfl3363@hanmail.net>
2023-05-26 17:42:52 +0900

    로딩상태 처리하기

--- 09.React_http_request/src/App.js
+++ 09.React_http_request/src/App.js
@@ -6,7 +6,10 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
+  const [isLoading,setIsLoading] = useState(false)
+
   async function fetchMoviesHandler() {
+    setIsLoading(true)
     const response = await fetch(`https://swapi.dev/api/films/`);
     const data = await response.json();
     const transforemdMovies = data.results.map((item) => {
@@ -19,6 +22,7 @@ function App() {
     });
 
     setMovies(transforemdMovies)
+    setIsLoading(false)
   }
 
   return (
@@ -27,7 +31,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length >0  && <MoviesList movies={movies} />}
+        {!isLoading && movies.length ===0  && <p>영화가 없습니다</p>}
+        {isLoading && <p>로딩중입니다</p>}
       </section>
     </React.Fragment>
   );
