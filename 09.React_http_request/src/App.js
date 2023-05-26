88191a395341f2f038539d59165e7a4628b66371
lulla-by <thfl3363@hanmail.net>
2023-05-26 17:21:41 +0900

    async/await 사용하기

--- 09.React_http_request/src/App.js
+++ 09.React_http_request/src/App.js
@@ -1,47 +1,25 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: "Some Dummy Movie",
-      openingText: "This is the opening text of the movie",
-      releaseDate: "2021-05-18",
-    },
-    {
-      id: 2,
-      title: "Some Dummy Movie 2",
-      openingText: "This is the second opening text of the movie",
-      releaseDate: "2021-05-19",
-    },
-  ];
-
   const [movies, setMovies] = useState([]);
 
+  async function fetchMoviesHandler() {
+    const response = await fetch(`https://swapi.dev/api/films/`);
+    const data = await response.json();
+    const transforemdMovies = data.results.map((item) => {
+      return {
+        id: item.episode_id,
+        title: item.title,
+        openingText: item.opening_crawl,
+        releaseDate: item.release_date,
+      };
+    });
 
-  function fetchMoviesHandler() {
-    fetch(`https://swapi.dev/api/films/`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map(movieData => {
-          return {
-            id:movieData.episode_id,
-            title:movieData.title,
-            openingText:movieData.opening_crawl,
-            releaseDate:movieData.release_date
-          }
-        });
-        setMovies(transformedMovies);
-       
-      });
+    setMovies(transforemdMovies)
   }
-
-
 
   return (
     <React.Fragment>
