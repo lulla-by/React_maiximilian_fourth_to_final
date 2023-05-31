5880aaed664341fb49314a85710f66f7a86098ae
lulla-by <thfl3363@hanmail.net>
2023-05-31 13:53:15 +0900

    사용자 정의 훅 구성하기

--- 10.React_CustomHook/src/hooks/use-counter.js
+++ 10.React_CustomHook/src/hooks/use-counter.js
@@ -1,16 +1,24 @@
-import { useEffect, useState } from 'react';
-const useCounter = () =>{
+import { useEffect, useState } from "react";
+
+
+// 매개변수와 인자를 받아서 custom hook이 사용되도록
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+    // 의존성이 변경될때마다 useEffect함수가 재실행되도록
+  }, [forwards]);
 
   return counter;
-}
+};
 
 export default useCounter;
