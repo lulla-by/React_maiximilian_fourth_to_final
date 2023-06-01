64ea219d4a0b384d6d1ac64675e86aa36989f629
lulla-by <thfl3363@hanmail.net>
2023-06-01 15:54:41 +0900

    기본검증 추가하기

--- 11.React_Form,input/src/components/SimpleInput.js
+++ 11.React_Form,input/src/components/SimpleInput.js
@@ -1,28 +1,33 @@
-import { useState, useRef } from 'react';
-
+import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
-  const [enteredName,setEnteredName] = useState("");  
-  const nameInputRef = useRef()
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputRef = useRef();
 
-  const nameInputChangeHandler = e =>{
-    setEnteredName(e.target.value)
-  }
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
 
-  const formSubmissionHandler = e =>{
-e.preventDefault();
-const enteredValue = nameInputRef.current.value
-console.log(enteredName);
-console.log(enteredValue);
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
 
-// nameInputRef.current.value = "" => Not ideal, Don't manipulate the DOM.
-setEnteredName("")
-}
+    if (enteredName.trim() === "") {
+      return;
+    }
+    const enteredValue = nameInputRef.current.value;
+    setEnteredName("");
+  };
   return (
-    <form onSubmit={formSubmissionHandler}> 
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} value={enteredName} onChange={nameInputChangeHandler} type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+          type="text"
+          id="name"
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
