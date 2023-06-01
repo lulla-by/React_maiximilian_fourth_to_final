94ff8bcce2b3dc152de7a20cc43da1b7902d0dca
lulla-by <thfl3363@hanmail.net>
2023-06-01 16:03:30 +0900

    검증 피드백 제공하기

--- 11.React_Form,input/src/components/SimpleInput.js
+++ 11.React_Form,input/src/components/SimpleInput.js
@@ -4,6 +4,9 @@ const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const nameInputRef = useRef();
 
+  // 검증을 위한  state추가, 기본 state를 false로 지정하여 초기값이 유효하지 않도록 설정,
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
   };
@@ -12,14 +15,19 @@ const SimpleInput = (props) => {
     e.preventDefault();
 
     if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
       return;
     }
+    // 위의 조건문을 통과하면 유효한것
+    setEnteredNameIsValid(true);
     const enteredValue = nameInputRef.current.value;
     setEnteredName("");
   };
+
+  const nameInputClasses = enteredNameIsValid ? "form-control":"form-control invalid"
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -28,6 +36,7 @@ const SimpleInput = (props) => {
           type="text"
           id="name"
         />
+        {!enteredNameIsValid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
