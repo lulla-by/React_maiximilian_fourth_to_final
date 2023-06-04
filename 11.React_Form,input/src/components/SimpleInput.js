e5b2e94bdeb2621b0184d72f110df60bfbe3493c
lulla-by <thfl3363@hanmail.net>
2023-06-04 16:16:11 +0900

    리팩토링 및 state파생

--- 11.React_Form,input/src/components/SimpleInput.js
+++ 11.React_Form,input/src/components/SimpleInput.js
@@ -1,76 +1,60 @@
-import { useState, useRef, useEffect } from "react";
+import { useState} from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-  const nameInputRef = useRef();
-
-  // 검증을 위한  state추가, 기본 state를 false로 지정하여 초기값이 유효하지 않도록 설정,
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   // 사용자가 입력란에 입력해서 enteredName이 있는지를 확인, 초기값을 falsfe로 설정
-  const [enteredNameTouched,setEnteredNameTouched] = useState(false)
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(()=>{
-    if(enteredNameIsValid){
-      console.log("Name input is valid!")
-    }
-  },[enteredNameIsValid])
+  // 우리가 해야할 것: 1.입력값이 유효한지, 2. 사용자가 입력창을 건드렸는지,
+  // 값이 유효하지 않고 입력창을 건드렸다면 => 사용자에게 에러 보여줌
+  // 값이 유효+ 입력창을 건드렸다면 => 사용자에게 에러 보여주지 않음
 
+  // 이미 존재하는 enteredName에서 얻기
+  const enteredNameIsValid = enteredName.trim() !== "";
+
+  // 입력창이 건드려진 뒤 + 값이 유효하지 않을때만 유효하지않게 판단
+  const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
-    // 조건문에 e.target.value를 넣는 이유
-    // 비록 setEnteredName으로 업데이트하고 있으나
-    // 이러한 상태들은 리액트에서 비동기적으로 처리되므로 즉각적으로 반영되지 않음
-    if (e.target.value.trim() !== "") {
-      setEnteredNameIsValid(true);
-    }
   };
 
-
-  const nameInputBlurHandeler=(e)=>{
+  const nameInputBlurHandeler = (e) => {
     setEnteredNameTouched(true);
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-    }
-  }
-
+  };
 
   const formSubmissionHandler = (e) => {
     e.preventDefault();
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-      console.log("불통과")
+    if (!enteredNameIsValid) {
       return;
     }
-    console.log("통과")
-    // 위의 조건문을 통과하면 유효한것
-    setEnteredNameIsValid(true);
-    // const enteredValue = nameInputRef.current.value;
-    setEnteredName("");
-  };
-                        // 입력창이 건드려진 뒤 + 값이 유효하지 않을때만 유효하지않게 판단
-  const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched;
- 
-  // nameInputIsInvalid가 true일때 적용되고 그렇지 않을 때 적용되지 않도록
-  const nameInputClasses = nameInputIsInValid ? "form-control invalid":"form-control"
 
+    setEnteredName("");
+    setEnteredNameTouched(false)
+  };
+
+  // nameInputIsInvalid가 true일때 적용되고 그렇지 않을 때 적용되지 않도록
+  const nameInputClasses = nameInputIsInValid
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          // ref={nameInputRef}
           value={enteredName}
           onChange={nameInputChangeHandler}
           onBlur={nameInputBlurHandeler}
           type="text"
           id="name"
         />
-        {nameInputIsInValid && <p className="error-text">Name must not be empty</p>}
+        {nameInputIsInValid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
