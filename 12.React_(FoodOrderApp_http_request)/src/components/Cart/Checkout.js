b5cea719998e5c834d921c9fbcecab3527c430c0
lulla-by <thfl3363@hanmail.net>
2023-06-08 20:36:38 +0900

    유효성 검사

--- 12.React_(FoodOrderApp_http_request)/src/components/Cart/Checkout.js
+++ 12.React_(FoodOrderApp_http_request)/src/components/Cart/Checkout.js
@@ -1,44 +1,103 @@
-import { useRef } from 'react'
-import classes from './Checkout.module.css'
+import { useRef, useState } from "react";
+import classes from "./Checkout.module.css";
+
+// 도우미 함수 작성(유효성 검사용)
+const isEmpty = (value) => value.trim() !== "";
+const isFiveChars = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
+  const [formInputValidity, setFormInputValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+
   const nameInput = useRef();
   const streetInput = useRef();
   const postalInput = useRef();
   const cityInput = useRef();
+
   const confirmHandler = (e) => {
     e.preventDefault();
+
     const enteredName = nameInput.current.value;
     const enteredStreet = streetInput.current.value;
     const enteredPostal = postalInput.current.value;
-    const enteredCity= cityInput.current.value;
-  }
+    const enteredCity = cityInput.current.value;
+
+    const enteredNameIsValid = isEmpty(enteredName);
+    const enteredStreetIsValid = isEmpty(enteredStreet);
+    const enteredCityIsValid = isEmpty(enteredCity);
+    const enteredPostalIsValid = isFiveChars(enteredPostal);
+
+    // 여러 필드의 유효성에 대한 정보를 줌
+    setFormInputValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalIsValid;
+    console.log(formIsValid);
+
+    if (!formIsValid) {
+      return;
+    }
+
+    //submit cart data
+  };
+
+  const nameControlClasses = `${classes.control} ${
+    formInputValidity.name ? "" : classes.invalid
+  }`;
+  const streetControlClasses = `${classes.control} ${
+    formInputValidity.street ? "" : classes.invalid
+  }`;
+  const postalCodeControlClasses = `${classes.control} ${
+    formInputValidity.postalCode ? "" : classes.invalid
+  }`;
+  const cityCodeControlClasses = `${classes.control} ${
+    formInputValidity.city ? "" : classes.invalid
+  }`;
+
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-    <div className={classes.control}>
-      <label htmlFor='name'>Your Name</label>
-      <input ref={nameInput} type='text' id='name' />
-    </div>
-    <div className={classes.control}>
-      <label htmlFor='street'>Street</label>
-      <input ref={streetInput} type='text' id='street' />
-    </div>
-    <div className={classes.control}>
-      <label htmlFor='postal'>Postal Code</label>
-      <input ref={postalInput} type='text' id='postal' />
-    </div>
-    <div className={classes.control}>
-      <label htmlFor='city'>City</label>
-      <input ref={cityInput} type='text' id='city' />
-    </div>
-    <div className={classes.actions}>
-      <button type='button' onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button className={classes.submit}>Confirm</button>
-    </div>
-  </form>
-  )
-}
+      <div className={nameControlClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input ref={nameInput} type="text" id="name" />
+        {!formInputValidity.name && <p>Please enter a valid name!</p>}
+      </div>
+      <div className={streetControlClasses}>
+        <label htmlFor="street">Street</label>
+        <input ref={streetInput} type="text" id="street" />
+        {!formInputValidity.street && <p>Please enter a valid street!</p>}
+      </div>
+      <div className={postalCodeControlClasses}>
+        <label htmlFor="postal">Postal Code</label>
+        <input ref={postalInput} type="text" id="postal" />
+        {!formInputValidity.postalCode && (
+          <p>Please enter a valid postal code (5 characters long)!</p>
+        )}
+      </div>
+      <div className={cityCodeControlClasses}>
+        <label htmlFor="city">City</label>
+        <input ref={cityInput} type="text" id="city" />
+        {!formInputValidity.city && <p>Please enter a valid city!</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
 
-export default Checkout
+export default Checkout;
