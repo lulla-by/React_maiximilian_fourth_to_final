a69da0a2e40cb43bf4b00cdba2cb40680cd7f792
lulla-by <thfl3363@hanmail.net>
2023-06-08 14:50:43 +0900

    http request

--- 12.React_(FoodOrderApp_http_request)/src/components/Meals/AvailableMeals.js
+++ 12.React_(FoodOrderApp_http_request)/src/components/Meals/AvailableMeals.js
@@ -1,10 +1,32 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
-  
-  const mealsList = DUMMY_MEALS.map((meal) => (
+
+  const [meals, setMeals] = useState([]);
+
+  const getData = async () => {
+    const resppnse = await fetch(`${process.env.REACT_APP_URL}meals.json`);
+    const data = await resppnse.json();
+    const loadedmeals = [];
+    for (const key in data) {
+      loadedmeals.push({
+        id: key,
+        name: data[key].name,
+        description: data[key].description,
+        price: data[key].price,
+      });
+    }
+    setMeals(loadedmeals);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
