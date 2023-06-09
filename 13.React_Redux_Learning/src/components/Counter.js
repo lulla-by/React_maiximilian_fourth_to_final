dd3f762b5636cd06a7eb228105b5ec1634cc290e
lulla-by <thfl3363@hanmail.net>
2023-06-09 16:54:56 +0900

    useDispatch() - 컴포넌트 내부에서 Action Dispatch

--- 13.React_Redux_Learning/src/components/Counter.js
+++ 13.React_Redux_Learning/src/components/Counter.js
@@ -1,14 +1,30 @@
-import { useSelector } from 'react-redux';
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const counter = useSelector(state => state.counter)
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   const toggleCounterHandler = () => {};
+
+  const incrementHandeler = () => {
+      dispatch({ type: "INCREMENT" });
+  };
+  const decrementHandeler = () => {
+      dispatch({ type: "DECREMENT" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button id="increment" onClick={incrementHandeler}>
+          Increment
+        </button>
+        <button id="decrement" onClick={decrementHandeler}>
+          Decrement
+        </button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
