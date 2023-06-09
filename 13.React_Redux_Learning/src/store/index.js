7ce5de6676b278bee3601abe3c3bc2029c633414
lulla-by <thfl3363@hanmail.net>
2023-06-09 17:56:36 +0900

    리덕스 state를 올바르게 사용하는 방법 - 원본 state를 건드리면 안된다.

--- 13.React_Redux_Learning/src/store/index.js
+++ 13.React_Redux_Learning/src/store/index.js
@@ -9,6 +9,22 @@ let initialState = {
 const counterReducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
     if (action.amount) {
+
+      // 리덕스에서는 이처럼 원본 state를 건드리면 안됌
+
+      // state.counter++
+
+      // return{
+      //   counter:state.counter,
+      //   showCounter:state.showCounter
+      // }
+
+      // 코드가 정상적으로 작동되는 것처럼 보이지만,
+      // 이럴 경우 예상못한 버그가 발생하거나 프로그램 디버깅이 어려워질 수 있음
+      // 버그가 발생하지 않더라도 state가 동기화되지 않는 더 큰 애플리케이션에서 예기치 않은 부작용이 생길 수 있고
+      // 갑자기 UI가 더이상 state를 정확히 반영하지 않을 수 있음
+
+
       return {
         counter: state.counter + action.amount,
         showCounter: state.showCounter,
