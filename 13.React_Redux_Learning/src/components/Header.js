a2770cb034b2233abfef0fcf6f925ab9117d987c
lulla-by <thfl3363@hanmail.net>
2023-06-09 21:31:49 +0900

    새 슬라이스에서 읽기 및  dispatch

--- 13.React_Redux_Learning/src/components/Header.js
+++ 13.React_Redux_Learning/src/components/Header.js
@@ -5,32 +5,27 @@ import { authActions } from "../store";
 const Header = () => {
   const auth = useSelector((state) => state.auth.isAuthenticated);
   const dispatch = useDispatch();
-  const loginHandeler = () => {
-    dispatch(authActions.login());
-  };
   const logoutHandeler = () => {
     dispatch(authActions.logout());
   };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">My Products</a>
-          </li>
-          <li>
-            <a href="/">My Sales</a>
-          </li>
-          <li>
-            {auth === true ? (
+      {auth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
               <button onClick={logoutHandeler}>Logout</button>
-            ) : (
-              <button onClick={loginHandeler}>Login</button>
-            )}
-          </li>
-        </ul>
-      </nav>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
