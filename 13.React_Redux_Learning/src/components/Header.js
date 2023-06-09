eed0e4efb8bc5463318ffed6b959b3ce8af1930a
lulla-by <thfl3363@hanmail.net>
2023-06-09 21:22:55 +0900

    다중 슬라이스 작업

--- 13.React_Redux_Learning/src/components/Header.js
+++ 13.React_Redux_Learning/src/components/Header.js
@@ -1,19 +1,33 @@
-import classes from './Header.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Header.module.css";
+import { authActions } from "../store";
 
 const Header = () => {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const loginHandeler = () => {
+    dispatch(authActions.login());
+  };
+  const logoutHandeler = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
           <li>
-            <a href='/'>My Products</a>
+            <a href="/">My Products</a>
           </li>
           <li>
-            <a href='/'>My Sales</a>
+            <a href="/">My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            {auth === true ? (
+              <button onClick={logoutHandeler}>Logout</button>
+            ) : (
+              <button onClick={loginHandeler}>Login</button>
+            )}
           </li>
         </ul>
       </nav>
