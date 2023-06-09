469918ce40eb31056d47542a4f81d6f75cabc9c6
lulla-by <thfl3363@hanmail.net>
2023-06-09 15:14:37 +0900

    구독 생성

--- 13.React_Redux/redux_demo.js
+++ 13.React_Redux/redux_demo.js
@@ -6,9 +6,20 @@ const counterReducer = (state,aciton) =>{
 
   // 일반적으로 출력값은 상태 객체, 이론적으로는 어떤 유형도 가능
   return {
-    counter: state.counter+1
+    counter: state.counter + 1
   };
 }
 
 // 저장소 생성 - 데이터 관리, 데이터는 리듀서 함수에 의해 결정됨(리듀서 함수가 새로운 상태 스냅샷을 생성할 것이기 때문)
 const store = redux.createStore(counterReducer);  // 어떤 리듀서가 그 저장소를 변경하는지 저장소에 알려줌
+
+
+//구독
+const counterSubscriber = ()=>{
+ const latesState =  store.getState()
+  //getState()는 createStore()로 생성된 저장소에서 사용할 수 있는 메서드, 업데이트 이후의 최신 스냅샷 제공
+  console.log(latesState);
+}
+
+// 리덕스가 이 구독함수를 인식하도록 하고 변경될 때마다 이 함수를 실행하라고 말해줘야 함
+store.subscribe(counterSubscriber);
