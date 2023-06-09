f323a736fb42169267612677f9e0f56a703036ec
lulla-by <thfl3363@hanmail.net>
2023-06-09 16:34:53 +0900

    리액트용 스토어 생성 - redux의 createStore 임포트 - store 생성 - counterReducer생성 및 스토어와 연결 - store export

--- 13.React_Redux_Learning/src/store/index.js
+++ 13.React_Redux_Learning/src/store/index.js
@@ -1,5 +1,26 @@
 // 리덕스 로직 저장
+import { createStore } from "redux";
+
+let initialState = {
+  counter: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  if(action.type === "INCREMENT"){
+    return {
+      counter:state.counter + 1
+    }
+  }
+  if(action.type === "DECREMENT"){
+    return {
+      counter:state.counter - 1
+    }
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
 
 
-
-
+export default store;
