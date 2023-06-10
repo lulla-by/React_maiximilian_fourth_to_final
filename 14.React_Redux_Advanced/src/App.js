36c41766575bb4f6303495b9a1141c5ee25149a0
lulla-by <thfl3363@hanmail.net>
2023-06-10 21:32:31 +0900

    액션 생성자 Thunk사용하기

--- 14.React_Redux_Advanced/src/App.js
+++ 14.React_Redux_Advanced/src/App.js
@@ -4,8 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiAcitons } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart";
 
 // 맨처음 컴포넌트 렌더링 상황에서는 notification컴포넌트롤 보여주지 않기 위해 설정
 // 초기 렌더링 이후에는 false로 설정하여 이후에 데이터 송신시에는 상태 처리 여부가 보여짐
@@ -20,50 +20,11 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      // App컴포넌트 상단에 표시될 데이터 송신후 처리여부를 위한 dispathc 설정(전송중)
-      dispatch(
-        uiAcitons.showNotification({
-          status: "pending",
-          title: "Sending..",
-          message: "Sending cart data!",
-        })
-      );
-
-      // 현재 cart의 값을 파이어베이스 /cart.json으로 전송
-      const response = await fetch(`${process.env.REACT_APP_URL}cart.json`, {
-        method: "PUT",
-        body: JSON.stringify(cart),
-      });
-
-      if (!response.ok) {
-        throw new Error("Sendig cart data faild");
-      }
-      // App컴포넌트 상단에 표시될 데이터 송신후 처리여부를 위한 dispathc 설정(성공)
-      dispatch(
-        uiAcitons.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
-    // app컴포넌트 외부에서 설정한 초기 notificaiton 컴포넌트 관련 작업
-    if (isInitial) {
+    if(isInitial){
       isInitial = false;
       return;
     }
-    // App컴포넌트 상단에 표시될 데이터 송신후 처리여부를 위한 dispathc 설정(에러발생)
-    sendCartData().catch((error) => {
-      dispatch(
-        uiAcitons.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed",
-        })
-      );
-    });
+    dispatch(sendCartData(cart))
   }, [cart, dispatch]);
   return (
     <Fragment>
--- 14.React_Redux_Advanced/src/store/cart.js
+++ 14.React_Redux_Advanced/src/store/cart.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiAcitons } from "./ui-slice";
 
 const initialState = {
   items: [],
@@ -10,6 +11,10 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuntity = action.payload.totalQuntity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, aciton) {
       const newItem = aciton.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -40,6 +45,56 @@ const cartSlice = createSlice({
     },
   },
 });
+
+// Thunk
+// 이 데이터는 다른 작업을 수행하지 않고 다른 함수인 비동기 함수를 즉시 반환함
+export const sendCartData = (cart) => {
+  // 반환된 이 비동기 함수에서 알림 작업을 dispatch
+  return async (dispatch) => {
+    // 전송중이라고 알림을 dispatch
+    dispatch(
+      uiAcitons.showNotification({
+        status: "pending",
+        title: "Sending..",
+        message: "Sending cart data!",
+      })
+    );
+
+// 그다음 새 함수를 만들어서 요청을 보내고
+    const sendRequest = async () => {
+      const response = await fetch(`${process.env.REACT_APP_URL}cart.json`, {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("Sendig cart data faild");
+      }
+    };
+
+    //에러를 잡기위해 try{} cathc{} 사용
+    try {
+      await sendRequest();
+      // 오류가 없으면 성공 알림을 dispatch
+      dispatch(
+        uiAcitons.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      // 오류가 있으면 오류 알림을 dispatch
+      dispatch(
+        uiAcitons.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
