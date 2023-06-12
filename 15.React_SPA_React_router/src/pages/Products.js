b013af947b0786bd862a33e56bb01c2cac1e27cc
lulla-by <thfl3363@hanmail.net>
2023-06-12 17:45:01 +0900

    동적 라우트에 링크 추가하기

--- 15.React_SPA_React_router/src/pages/Products.js
+++ 15.React_SPA_React_router/src/pages/Products.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  { id: "p1", title: "product 1" },
+  { id: "p2", title: "product 2" },
+  { id: "p3", title: "product 3" },
+];
 
 const ProductsPage = () => {
   return (
     <>
-    <h1>Products Page</h1>
-    <ul>
-      <li>product 1</li>
-      <li>product 2</li>
-      <li>product 3</li>
+      <h1>Products Page</h1>
+      <ul>
+        {PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
-  )
-}
+  );
+};
 
 export default ProductsPage;
