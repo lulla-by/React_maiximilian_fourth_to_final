18218b805a9664b51ea12c1ec5d2a9a1cec09abf
lulla-by <thfl3363@hanmail.net>
2023-06-12 14:30:41 +0900

    라우트를 정의하는 다른 방법

--- 15.React_SPA_React_router/src/App.js
+++ 15.React_SPA_React_router/src/App.js
@@ -1,11 +1,20 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider, createRoutesFromElements,Route} from "react-router-dom";
+
 import { Home } from "./pages/Home";
 import Products from "./pages/Products";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/products", element: <Products /> },
-]);
+const routeDefinitions = createRoutesFromElements(
+<Route>
+  <Route  path= "/" element= {<Home /> }/>
+  <Route  path= "/products" element= { <Products /> }/>
+</Route>
+);
+
+// const router = createBrowserRouter([
+//   { path: "/", element: <Home /> },
+//   { path: "/products", element: <Products /> },
+// ]);
+const router = createBrowserRouter(routeDefinitions)
 
 function App() {
   return <RouterProvider router={router} />;
