ce1d5218a358dc037624059fcf9e4a4c12937ef5
lulla-by <thfl3363@hanmail.net>
2023-06-12 17:12:12 +0900

    네비게이션 링크 사용하기

--- 15.React_SPA_React_router/src/components/MainNavigation.js
+++ 15.React_SPA_React_router/src/components/MainNavigation.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import classes from "./MainNavigation.module.css"
+import { NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 export const MainNavigation = () => {
   return (
     <header className={classes.header}>
       <nav>
-        <ul  className={classes.list}>
+        <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
