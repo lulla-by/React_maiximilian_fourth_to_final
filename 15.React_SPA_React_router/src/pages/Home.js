20b74d1cdffe340e2a2b8fba565e5ba355486c0c
lulla-by <thfl3363@hanmail.net>
2023-06-12 14:44:09 +0900

    Link로 페이지간 이동

--- 15.React_SPA_React_router/src/pages/Home.js
+++ 15.React_SPA_React_router/src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import Products from './Products'
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <h1>Home</h1>
+    <Link to="/products"> 
+    <p>Go to the list of products</p>
+    </Link>
+    </>
   )
 }
+ 
