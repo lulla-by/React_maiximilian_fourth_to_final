fc59f007256ad40779648709268724d0054d5f1d
lulla-by <thfl3363@hanmail.net>
2023-06-13 14:34:27 +0900

    오류데이터를 추출하고 응답 내보내기

--- 15-2.React_SPA_project/frontend/src/pages/Error.js
+++ 15-2.React_SPA_project/frontend/src/pages/Error.js
@@ -1,5 +1,29 @@
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
 const ErrorPage = () => {
-  return <h1>An error occured!</h1>;
+  const error = useRouteError();
+  let title = "An error occured";
+  let message = "Something went wrong!";
+
+  //  유효하지 않은 주소를 입력할 경우
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  // /events를 방문해서 오류가 날 경우
+  if (error.status === 404) {
+    message = "Could not find resource or page!";
+  }
+  return (
+    <>
+    <MainNavigation />
+    <PageContent title={title}>
+      <p>{message}</p>
+    </PageContent>
+    </>
+  );
 };
 
 export default ErrorPage;
--- 15-2.React_SPA_project/frontend/src/pages/Events.js
+++ 15-2.React_SPA_project/frontend/src/pages/Events.js
@@ -14,10 +14,11 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/dsf");
+  const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
     // return {isError:true,message:"Could not fetch events."}
-    throw new Error("")
+    // new Response()를 생성해서 이것을 throw
+    throw new Response(JSON.stringify({message:"could not fetch events."}),{status:500})
   } else {
     return response;
     // const resData = await response.json();
