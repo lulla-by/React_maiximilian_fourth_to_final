13ff2db31c281c11d7cfa41125f7da20f2d0960c
lulla-by <thfl3363@hanmail.net>
2023-06-13 19:46:18 +0900

    완성

--- 15-2.React_SPA_project/frontend/src/pages/Home.js
+++ 15-2.React_SPA_project/frontend/src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import PageContent from '../components/PageContent';
 
 const HomePage = () => {
   return (
-    <h1>Home</h1>
+    <PageContent title="Welcome">
+      <p>멋진 이벤트를 탐색해보세요 ㅎ</p>
+    </PageContent>
   )
 }
 
