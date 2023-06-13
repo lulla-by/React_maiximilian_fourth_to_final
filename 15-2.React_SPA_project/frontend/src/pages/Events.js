adff28b10de53ee526f75e1efc133df44edaf861
lulla-by <thfl3363@hanmail.net>
2023-06-13 19:12:05 +0900

    defer함수로 데이터 가져오기를 연기하는 방법

--- 15-2.React_SPA_project/frontend/src/pages/Events.js
+++ 15-2.React_SPA_project/frontend/src/pages/Events.js
@@ -1,19 +1,24 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
   const data = useLoaderData();
   const events = data.events;
 
-  // if(data.isError){
-  //   return <p>{data.message}</p>
-  // }
-  return <>{<EventsList events={events} />}</>;
+  return(
+    <Suspense fallback={<p style={{textAlign:'center'}}>Loading...</p>}>
+    <Await resolve={events}>
+      {
+        (loadedEvents) =><EventsList events={loadedEvents}/>
+      }
+    </Await>
+    </Suspense>
+  )
 }
 
 export default EventsPage;
-
-export async function loader() {
+async function loadEvents (){
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
     // return {isError:true,message:"Could not fetch events."}
@@ -21,8 +26,15 @@ export async function loader() {
     // throw new Response(JSON.stringify({message:"could not fetch events."}),{status:500})
     throw json({ message: "could not fetch events." }, { status: 500 });
   } else {
-    return response;
+   
+    const resData = await response.json();
+    return resData.events
     // const resData = await response.json();
     // return resData.events;
   }
+}
+export function loader() {
+return defer({
+ events:loadEvents()
+})
 }
