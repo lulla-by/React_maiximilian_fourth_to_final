7e03c8695312e24e80198210c1d62c74f6b4e2f4
lulla-by <thfl3363@hanmail.net>
2023-06-13 14:02:42 +0900

    커스텀 오류를 이용한 오류처리

--- 15-2.React_SPA_project/frontend/src/pages/Events.js
+++ 15-2.React_SPA_project/frontend/src/pages/Events.js
@@ -2,19 +2,25 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events
+
+  // if(data.isError){
+  //   return <p>{data.message}</p>
+  // }
   return <>{<EventsList events={events} />}</>;
 }
 
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/dsf");
   if (!response.ok) {
-    // ...
+    // return {isError:true,message:"Could not fetch events."}
+    throw new Error("")
   } else {
-    const resData = await response.json();
-    console.log(resData);
-    return resData.events;
+    return response;
+    // const resData = await response.json();
+    // return resData.events;
   }
 }
