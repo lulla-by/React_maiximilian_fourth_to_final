0dc8c224f3621442d4db28e3a408b54801e73e96
lulla-by <thfl3363@hanmail.net>
2023-06-15 14:21:11 +0900

    지연로딩 추가하기 - 특정 컴포넌트를 나중에 불러오는 것

--- 17.React_Deploying/src/App.js
+++ 17.React_Deploying/src/App.js
@@ -1,13 +1,17 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import로 불러오면 항상 실행
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+// import PostPage, { loader as postLoader } from "./pages/Post";
+import HomePage from "./pages/Home";
+import RootLayout from "./pages/Root";
+import { lazy, Suspense } from "react";
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
-import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
-import RootLayout from './pages/Root';
-
+const BlogPage = lazy(() => import("./pages/Blog"));
+// lazy 함수는 동적으로 임포트하는 함수를 인자로 받음
+const PostPage = lazy(() => import("./pages/Post"));
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       {
@@ -15,10 +19,29 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'posts',
+        path: "posts",
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>
+            ),
+            // 이 loader함수는 블로그페이지의 loader가 호출될 때만 실행
+            loader: () =>
+              import("./pages/Blog").then((module) => module.loader()),
+          },
+          {
+            path: ":id",
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <PostPage />
+              </Suspense>
+            ),
+            loader: (meta) =>
+              import("./pages/Post").then((module) => module.loader(meta)),
+          },
         ],
       },
     ],
