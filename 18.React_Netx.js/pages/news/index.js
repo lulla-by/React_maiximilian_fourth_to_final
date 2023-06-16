57887c957d42e6d2f337e48523d663201b50b6cf
lulla-by <thfl3363@hanmail.net>
2023-06-16 18:39:18 +0900

    페이지 간 연결하기

--- 18.React_Netx.js/pages/news/index.js
+++ 18.React_Netx.js/pages/news/index.js
@@ -1,8 +1,23 @@
+import { Fragment } from "react";
+import Link from 'next/link'
+
+
 // our domain.com/news
 
-
 function NewsPage() {
-  return <h1>the news page</h1>
+  return (
+    <Fragment>
+      <h1>the news page</h1>
+      <ul>
+        <Link href="/news/nextjs">
+          <li>NextJS</li>
+        </Link>
+        <Link href="/news/something">
+          <li>something</li>
+        </Link>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
