d5af0ced43cf82cf251a2c0f4d2a0f4e28c0e5e6
lulla-by <thfl3363@hanmail.net>
2023-06-16 19:17:17 +0900

    list 출력하기

--- 18-2.React_NextJS_bigger-project/pages/index.js
+++ 18-2.React_NextJS_bigger-project/pages/index.js
@@ -1,3 +1,21 @@
-function name(params) {
-  
+import MeetupList from "../components/meetups/MeetupList";
+
+function HomePage() {
+  console.log("확인");
+  const DUMMY_MEETUPS = [
+    {
+      id:"m1",title:"first",description:"firs",address:"...1",image:"https://image.newsis.com/2022/06/01/NISI20220601_0001011091_web.jpg"
+    },
+    {
+      id:"m2",title:"second",description:"second",address:"...2",image:"https://i.namu.wiki/i/VOh8auDwOK56oeXI6nmEeHjYNUJ2uW-oio5Otb-jB7PK5hxnUEvKz-8ItNsKuPSKyfJCP5I1nsn2iaSm9Z2CYA.webp"
+    },
+  ]
+
+  return (
+    <>
+    <MeetupList meetups={DUMMY_MEETUPS}/>
+    </>
+  );
 }
+
+export default HomePage;
