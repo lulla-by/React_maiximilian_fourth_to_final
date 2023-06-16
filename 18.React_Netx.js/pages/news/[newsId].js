527e5d6714ee76fad471f999b165494aeddfd4ac
lulla-by <thfl3363@hanmail.net>
2023-06-16 18:28:32 +0900

    동적 매개변수 값 불러오기 - useRouter 사용

--- 18.React_Netx.js/pages/news/[newsId].js
+++ 18.React_Netx.js/pages/news/[newsId].js
@@ -1,6 +1,13 @@
+import { useRouter } from "next/router";
+
 // our-domail.com/news/something-important
-function DetailPage () {
-  return <h1>The Detail Page</h1>
+
+function DetailPage() {
+  const router = useRouter();
+  console.log(router.query.newsId);
+  // send request to the backendAPI
+  // to fetch the news item width neswId
+  return <h1>The Detail Page</h1>;
 }
 
 export default DetailPage;
