7e7567e1c5dfc96823d05581ae246df1380a7898
lulla-by <thfl3363@hanmail.net>
2023-06-16 17:57:03 +0900

    Next.js 학습 초기 세팅 완료

---
+++ 18.React_Netx.js/pages/index.js
@@ -0,0 +1,8 @@
+// our domain.com/
+
+
+function HomePage() {
+  return <h1>the home page</h1>
+}
+
+export default HomePage;
---
+++ 18.React_Netx.js/pages/news.js
@@ -0,0 +1,8 @@
+// our domain.com/news
+
+
+function NewsPage() {
+  return <h1>the news page</h1>
+}
+
+export default NewsPage;
