d56bc6788c9c7c4481f69de6ae293286a0f68fb8
lulla-by <thfl3363@hanmail.net>
2023-06-18 00:28:07 +0900

    사용자 정의 컴포넌트 및 css 모듈 추가

---
+++ 18-2.React_NextJS_bigger-project/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return(
+    <MeetupDetail
+      image="https://image.newsis.com/2022/06/01/NISI20220601_0001011091_web.jpg"
+      title =" A First Meetup"
+      address="Some"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
