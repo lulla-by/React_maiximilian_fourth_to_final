4a3357852cf8f55b999d5ef5fefe2656ea397249
lulla-by <thfl3363@hanmail.net>
2023-06-18 15:52:18 +0900

    getStaticPaths로 경로 준비 및 대체 페이지 작업하기

--- 18-2.React_NextJS_bigger-project/pages/[meetupId]/index.js
+++ 18-2.React_NextJS_bigger-project/pages/[meetupId]/index.js
@@ -12,9 +12,33 @@ function MeetupDetails(props) {
   );
 }
 
+export async function getStaticPaths() {
+  // 동적페이지에서 필요한 함수
+  // NextJS에게 어떤 동적 매개변수 밸류의 어떤 페이지가 프리제너레이트 되어야 하는지 말해줌
+  return {
+    // 모든 동적 세그먼트 밸류가 있는 객체를 리턴
+    fallback: false,
+    // false로 설정하면 유효하지 않은 페이지에 대해 404error를 보여줌
+    // true로 설정하면  유효하지 않은 페이지에 대해 동적으로 페이지를 생성해서 보여줌
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
   console.log(context);
+  // 잠겨져 있어서 클라이언트 콘솔에서 확인 x, 터미널에서 확인 가능
   return {
     props: {
       meetupData: {
