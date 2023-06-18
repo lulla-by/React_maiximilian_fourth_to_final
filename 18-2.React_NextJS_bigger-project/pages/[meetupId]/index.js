74a1366860da128fc5f03355f5ccdceadd4c7187
lulla-by <thfl3363@hanmail.net>
2023-06-18 15:37:48 +0900

    SSG데이터를 가져오기 위한 params작업

--- 18-2.React_NextJS_bigger-project/pages/[meetupId]/index.js
+++ 18-2.React_NextJS_bigger-project/pages/[meetupId]/index.js
@@ -1,14 +1,32 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { useRouter } from "next/router";
 
-function MeetupDetails() {
-  return(
+function MeetupDetails(props) {
+  return (
     <MeetupDetail
       image="https://image.newsis.com/2022/06/01/NISI20220601_0001011091_web.jpg"
-      title =" A First Meetup"
+      title=" A First Meetup"
       address="Some"
       description="The meetup description"
     />
   );
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(context);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://image.newsis.com/2022/06/01/NISI20220601_0001011091_web.jpg",
+        id: meetupId,
+        title: " A First Meetup",
+        address: "Some",
+        description: "The meetup description",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
