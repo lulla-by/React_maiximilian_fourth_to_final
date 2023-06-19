9da5d878614009e59526e7d9de4c5bed4d73da2f
lulla-by <thfl3363@hanmail.net>
2023-06-19 16:56:18 +0900

    세부 정보 가져오기 및 페이지 준비

--- 18-2.React_NextJS_bigger-project/pages/[meetupId]/index.js
+++ 18-2.React_NextJS_bigger-project/pages/[meetupId]/index.js
@@ -1,54 +1,55 @@
+import { MongoClient,ObjectId } from 'mongodb';
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 import { useRouter } from "next/router";
 
 function MeetupDetails(props) {
+  const {image, title,address,description} = props.meetupData
   return (
     <MeetupDetail
-      image="https://image.newsis.com/2022/06/01/NISI20220601_0001011091_web.jpg"
-      title=" A First Meetup"
-      address="Some"
-      description="The meetup description"
+      image={image}
+      title={title}
+      address={address}
+      description={description}
     />
   );
 }
 
 export async function getStaticPaths() {
-  // 동적페이지에서 필요한 함수
-  // NextJS에게 어떤 동적 매개변수 밸류의 어떤 페이지가 프리제너레이트 되어야 하는지 말해줌
+
+  const client = await MongoClient.connect(process.env.REACT_MONGODB);
+  const db = client.db()
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({},{_id:1}).toArray() //_id:1 => ID만 포함하고 다른 필드값은 포함하지 않는다는 뜻
+
+  client.close()
+
   return {
     // 모든 동적 세그먼트 밸류가 있는 객체를 리턴
     fallback: false,
     // false로 설정하면 유효하지 않은 페이지에 대해 404error를 보여줌
     // true로 설정하면  유효하지 않은 페이지에 대해 동적으로 페이지를 생성해서 보여줌
-    paths: [
-      //paths는 array, array안에는 여러 객체가 있어야함, 동적 페이지 버전당 객체가 하나씩
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map(meetup=>({
+      params:{meetupId:meetup._id.toString()}
+    }))
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
-  console.log(context);
-  // 잠겨져 있어서 클라이언트 콘솔에서 확인 x, 터미널에서 확인 가능
+
+  const client = await MongoClient.connect(process.env.REACT_MONGODB);
+  const db = await client.db()
+  const meetupsCollection = await db.collection('meetups');
+  const selectedMeetup = await meetupsCollection.findOne({_id: new ObjectId(meetupId)})
   return {
     props: {
       meetupData: {
-        image:
-          "https://image.newsis.com/2022/06/01/NISI20220601_0001011091_web.jpg",
+        image:selectedMeetup.image,
         id: meetupId,
-        title: " A First Meetup",
-        address: "Some",
-        description: "The meetup description",
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
