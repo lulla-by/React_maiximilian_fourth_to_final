7ebd3c067d751b551bba6f5e3445d7af66a264ec
lulla-by <thfl3363@hanmail.net>
2023-06-19 16:22:19 +0900

    API경로로 http요청하기

--- 18-2.React_NextJS_bigger-project/pages/api/new-meetup.js
+++ 18-2.React_NextJS_bigger-project/pages/api/new-meetup.js
@@ -1,29 +1,20 @@
-import { MongoClient } from "mongodb";
-import { json } from 'react-router-dom';
-//서버사이드 코드를 포함하는 함수를 정의할 것
-//api라우트는 서버에서만 돌아감, 클라이언트에서는 아님
-// api 라우트에서 인증서를 사용할 수 있고 이 함수들은 라우트에 요청이 들어올 때마다 트리거되어 /api/new-meetup을 입력함
+import { MongoClient } from 'mongodb';
 
-// 이 페이지의 URL: /api/new-meetup
-
-// req: 들어오는 요청에 관한 데이터
-// res: 응답을 보낼때 필요함
 async function handler(req, res) {
-  //req에서 헤더나 요청 바디를 받을 수 있음
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const data = req.body;
-    const { title, image, address, description } = data;
+    
     const client = await MongoClient.connect(process.env.REACT_MONGODB);
-    console.log(client);
     const db = client.db();
-
-    const meetupsCollection = db.collection("meetups");
-    const result = await meetupsCollection.insertOne(data);
-    console.log(result)
-    client.close()
-
-    res.status(201).JSON({message:"Meetup inserted!"});
+    const meetupsCollection = db.collection('meetups');
+    
+    const result = await meetupsCollection.insertOne(data); // 데이터베이스에 문서 추가
+    
+    console.log(result);
+    
+    client.close(); // 데이터베이스 연결 차단
+    res.status(201).json({ message: 'Meetup inserted!' }); // 응답 반환
   }
 }
 
-export default handelr;
+export default handler;
--- 18-2.React_NextJS_bigger-project/pages/new-meetup/index.js
+++ 18-2.React_NextJS_bigger-project/pages/new-meetup/index.js
@@ -1,12 +1,23 @@
 import NewMeetupForm from "./../../components/meetups/NewMeetupForm";
+import { useRouter } from 'next/router';
 function NewMeetUpPage(params) {
-  const addMeetupHandelr =(data)=>{
-    console.log(data);
-
+ const router = useRouter()
+  const fetchUrl = '/api/new-meetup'
+  async function addMeetupHandelr(enteredData) {
+    const response = await fetch('/api/new-meetup', {
+			method: 'POST',
+			body: JSON.stringify(enteredData),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+    const data = await response.json();
+    router.push("/")
+    
   }
   return (
     <>
-      <NewMeetupForm onAddMeetup={addMeetupHandelr}/>
+      <NewMeetupForm onAddMeetup={addMeetupHandelr} />
     </>
   );
 }
