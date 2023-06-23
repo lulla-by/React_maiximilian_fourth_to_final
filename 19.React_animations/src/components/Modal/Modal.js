41f0ad6947944f3a544a80f2763bb7c9b056755f
lulla-by <thfl3363@hanmail.net>
2023-06-23 12:29:34 +0900

    CSS 클래스 이름 사용자 정의 -classNames에 객체를 전달하여 수동으로 정의

--- 19.React_animations/src/components/Modal/Modal.js
+++ 19.React_animations/src/components/Modal/Modal.js
@@ -18,7 +18,15 @@ const modal = (props) => {
       timeout={animationTiming}
       // CSSTransition컴포넌트를 통해 css 클래스를 순회 
       // fade-slide라는 몸통을 설정해주면 해당 클래스를 순회함
-      classNames="fade-slide">
+      // classNames="fade-slide"
+      // 클래스 이름 사용자 정의, 객체를 전달하여 다양한 css클래스들을 수동으로 정의
+      classNames={{
+        enter:"",
+        enterActive:"ModalOpen",
+        exit:"",
+        exitActive:"ModalClosed"
+      }}
+      >
       <div className="Modal">
         <h1>A Modal</h1>
         <button className="Button" onClick={props.closed}>
