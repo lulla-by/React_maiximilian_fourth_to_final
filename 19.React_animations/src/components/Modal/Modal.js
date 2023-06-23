f00210b758880de9ad0fd11ee563f03218ee8565
lulla-by <thfl3363@hanmail.net>
2023-06-23 11:58:20 +0900

    애니메이션 타이밍

--- 19.React_animations/src/components/Modal/Modal.js
+++ 19.React_animations/src/components/Modal/Modal.js
@@ -4,15 +4,22 @@ import Transition from "react-transition-group/Transition"
 
 import "./Modal.css";
 
+const animationTiming = {
+  enter: 400, // 요소를 나타내는데 걸리는 ㄴ시간
+  exit: 1000 // 요소를 사라지게하는데 걸리는 시간
+}
 const modal = (props) => {
 
   return (
-    <Transition in={props.show} timeout={500} mountOnEnter unmountOnExit>
+    <Transition in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit>
       {state => {
-          const cssClasses = [
-            "Modal",
-            state==="entering" ? "ModalOpen": state ==="exiting"?"ModalClosed":null
-          ];
+        const cssClasses = [
+          "Modal",
+          state === "entering" ? "ModalOpen" : state === "exiting" ? "ModalClosed" : null
+        ];
         return (
           <div className={cssClasses.join(" ")}>
             <h1>A Modal</h1>
