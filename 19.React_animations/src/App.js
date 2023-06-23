54ffdd9faf4b705138c306c005631ffc0f9df4e8
lulla-by <thfl3363@hanmail.net>
2023-06-23 11:44:40 +0900

    전환 컴포넌트 사용하기

--- 19.React_animations/src/App.js
+++ 19.React_animations/src/App.js
@@ -23,28 +23,30 @@ class App extends Component {
         <h1>React Animations</h1>
         <button className="Button" onClick={prevState => this.setState(prevState => ({ showBlock: !prevState.showBlock }))}>Toggle</button>
         <br />
-        {/* {this.state.showBlock ? */}
-        <Transition 
-        in={this.state.showBlock} 
-        timeout={1000}
-        mountOnEnter
-        unmountOnExit
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
         >
           {state => (
             <div
-             style={{
-              backgroundColor: "red", 
-              width: 100,
-              height: 100, 
-              margin: "auto",
-              transition:"opacity 1s ease-out",
-              opacity: state === "exiting" ? 0 : 1
-            }} 
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1
+              }}
             ></div>
           )}
         </Transition>
-        {/* : null} */}
-        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
+        <Transition in={this.state.modalIsOpen} timeout={500} mountOnEnter unmountOnExit>
+          {state => (
+            <Modal show={state} closed={this.closeModal} />
+          )}
+        </Transition>
         {this.state.modalIsOpen ? < Backdrop show={this.state.modalIsOpen} /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
