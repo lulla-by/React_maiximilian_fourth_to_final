41e5f10a163eacdc17d356812da7e148d41e5341
lulla-by <thfl3363@hanmail.net>
2023-07-02 16:04:34 +0900

    여러  state를 분리하여 관리

--- 23.React_React_hooks/src/components/Ingredients/IngredientForm.js
+++ 23.React_React_hooks/src/components/Ingredients/IngredientForm.js
@@ -5,10 +5,12 @@ import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
 
-  const [inputState,setInputState] = useState({ title: "", amount: "" }) //input 요소의 값은 언제나 문자열이라 관리차원으로 빈 스트링 적용
+  const [enteredTitle,setEnteredTitle] = useState( "") //input 요소의 값은 언제나 문자열이라 관리차원으로 빈 스트링 적용
+  const [enteredAmount,setEnteredAmount] = useState( "")
   const submitHandler = event => {
     event.preventDefault();
-    console.log(inputState);
+    console.log(enteredTitle);
+    console.log(enteredAmount);
   };
 
   return (
@@ -17,17 +19,15 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState.title} 
+            <input type="text" id="title" value={enteredTitle} 
             onChange={(event) => { 
-              const newTitle = event.target.value
-              setInputState((prevInputState)=>({ title: newTitle, amount: prevInputState.amount })) }} />
+              setEnteredTitle(event.target.value) }} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState.amount} 
+            <input type="number" id="amount" value={enteredAmount} 
             onChange={(event) => {
-              const newAmount = event.target.value
-              setInputState((prevInputState)=>({ title:prevInputState.title, amount:newAmount})) }} />
+              setEnteredAmount(event.target.value) }} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
