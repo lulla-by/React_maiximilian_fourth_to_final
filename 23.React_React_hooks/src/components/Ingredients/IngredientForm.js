f75c7b4e541a71abf8f80dbc03487d23ee1776ec
lulla-by <thfl3363@hanmail.net>
2023-07-02 15:53:23 +0900

    useState() - useState()시작하기 - useState() 및 state업데이트에 대한 추가 정보

--- 23.React_React_hooks/src/components/Ingredients/IngredientForm.js
+++ 23.React_React_hooks/src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+
+  const inputState = useState({ title: "", amount: "" }) //input 요소의 값은 언제나 문자열이라 관리차원으로 빈 스트링 적용
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    console.log(inputState[0]);
   };
 
   return (
@@ -15,11 +17,17 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={inputState[0].title} 
+            onChange={(event) => { 
+              const newTitle = event.target.value
+              inputState[1]((prevInputState)=>({ title: newTitle, amount: prevInputState.amount })) }} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={inputState[0].amount} 
+            onChange={(event) => {
+              const newAmount = event.target.value
+              inputState[1]((prevInputState)=>({ title:prevInputState.title, amount:newAmount})) }} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
